1f7f602afef0525bbb0cc5edb4a4965e26db1f02
Tom Zheng <77714331+tomzheng2001@users.noreply.github.com>
2021-11-22 00:22:37 -0800

    new changes

--- src/Components/Background.js
+++ src/Components/Background.js
@@ -3,65 +3,83 @@ import Particles from "react-tsparticles";
 
 const Background = () => {
     return <Particles id="tsparticles" options={{
-        fpsLimit: 30,
-        interactivity: {
-          detect_on: 'window',
-          events: {
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            repulse: {
-              distance: 150,
-              duration: 0.4,
-            },
-          },
-        },
         particles: {
-          color: {
-            value: "#ffffff",
-          },
-          links: {
-            color: "#ff7f51",
-            distance: 150,
-            enable: true,
-            opacity: 0.35,
-            width: 3,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "out",
-            random: false,
-            speed: 2.5,
-            straight: false,
-            bounce: false
-          },
-          number: {
-            density: {
-              enable: true,
-              value_area: 800,
+            number: {
+              value: 80,
+              density: {
+                enable: true,
+                area: 800
+              }
             },
-            value: 80,
+            color: {
+              value: ["#ff7f51"]
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: "#b6b2b2"
+              }
+            },
+            opacity: {
+              value: 0.5211089197812949,
+              random: false,
+              animation: {
+                enable: true,
+                speed: 2,
+                minimumValue: 0.1,
+                sync: false
+              }
+            },
+            size: {
+              value: 8.017060304327615,
+              random: true,
+              animation: {
+                enable: true,
+                speed: 12.181158184520175,
+                minimumValue: 0.1,
+                sync: false
+              }
+            },
+            lineLinked: {
+              enable: false,
+              distance: 150,
+              color: "#ff7f51",
+              opacity: 0.4,
+              width: 1
+            },
+            move: {
+              enable: true,
+              speed: 1,
+              direction: "none",
+              random: false,
+              straight: false,
+              outMode: "bounce",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+              }
+            }
           },
-          opacity: {
-            value: 0.25,
+          interactivity: {
+            detectOn: "canvas",
+            events: {
+              onHover: {
+                enable: true,
+                mode: "connect"
+              },
+              resize: true
+            },
+            modes: {
+              connect: {
+                  distance: 100,
+                  radius: 150,
+              },
+            }
           },
-          shape: {
-            type: "star",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: false,
+          detectRetina: false,
       }}/>
 }
 
