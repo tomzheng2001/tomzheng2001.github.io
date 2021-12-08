e5d2c8fe30f535ef83e536552d60e2adac3c2d4c
Tom Zheng <tomzheng@berkeley.edu>
2021-12-07 16:52:31 -0800

    update resume

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,22 +1,27 @@
 import React from "react";
 
 const Card = (props) => {
-
-
     return (
         <div className="card">
-            <h1 className="card__header">
-                {props.header}
-            </h1>
+            <h1 className="card__header">{props.header}</h1>
             <hr />
-            <p className="card__description">
-                {props.description}
-            </p>
+            <p className="card__description">{props.description}</p>
             <div className="card__buttons">
-                <a href={props.codelink} rel="noreferrer" target="_blank" className="card__buttons-icon">
+                <a
+                    href={props.codelink}
+                    rel="noreferrer"
+                    target="_blank"
+                    className="card__buttons-icon"
+                >
                     Code <i class="fab fa-github-square"></i>
                 </a>
-                <a href={props.sitelink} rel="noreferrer" target="_blank" className="card__buttons-icon">
+                <a
+                    href={props.sitelink}
+                    rel="noreferrer"
+                    target="_blank"
+                    className="card__buttons-icon"
+                    id="liveversion"
+                >
                     Site <i class="fas fa-window-restore"></i>
                 </a>
             </div>
